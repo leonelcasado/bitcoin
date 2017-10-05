6a2c3bb56d99c80d59b07f3f901a741ed2c27812
Leonel Casado <leonelcasado@gmail.com>
2017-10-05 12:01:47 -0300

    a

--- bitcoin/backend/historico/cotacao_fox_bit.py
+++ bitcoin/backend/historico/cotacao_fox_bit.py
@@ -4,7 +4,27 @@ from __future__ import unicode_literals
 import requests
 from datetime import datetime
 import time
-#from bitcoin.backend.historico.historico_bitcoin import gravar
+import pymysql
+from bitcoin import settings
+#==================================================
+def gravar(val_alta,val_baixa,val_compra,val_venda,val_volume):
+    try:
+        conn = pymysql.connect(host=settings.HOST_DB,port=3306, user=settings.USER_DB, passwd=settings.PASSWORD_DB, db=settings.NAME_DB)
+        cur = conn.cursor()
+        data_criacao=datetime.datetime.today()
+        valor_diferenca_alta_baixa=val_alta-val_baixa
+        valor_diferenca_compra_baixa=val_compra-val_baixa
+        valor_diferenca_venda_baixa=val_venda-val_baixa
+        sql="insert into historico_historico(valor_alta,valor_baixa,valor_compra,valor_venda,valor_volume,data_criacao,valor_diferenca_alta_baixa,valor_diferenca_compra_baixa,valor_diferenca_venda_baixa)values('%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (val_alta,val_baixa,val_compra,val_venda,val_volume,data_criacao,valor_diferenca_alta_baixa,valor_diferenca_compra_baixa,valor_diferenca_venda_baixa)
+        #print(sql)
+        cur.execute(sql)
+        conn.commit()
+    except Exception as e:
+        #print(e)
+        conn.rollback()
+    
+    cur.close()
+    conn.close()
 #==================================================
 def consultar_indicadores_bitcoin():
     try:
@@ -14,14 +34,14 @@ def consultar_indicadores_bitcoin():
         #print(json_data)
         #==================================================
         print('=======================================')
-        print('Início:',datetime.now().strftime('%d/%m/%Y %H:%M:%S'))
+        print('Início:',datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S'))
         print('Alta:%s' % json_data['high'])
         print('Baixa:%s' % json_data['low'])
         print('Compra:%s' % json_data['buy'])
         print('Venda:%s' % json_data['sell'])
         print('Volume:%s' % json_data['vol'])        
         #==================================================
-        #gravar(json_data['high'],json_data['low'],json_data['buy'],json_data['sell'],json_data['vol'])
+        gravar(json_data['high'],json_data['low'],json_data['buy'],json_data['sell'],json_data['vol'])
         #==================================================
     except Exception as e:
         print(e)
